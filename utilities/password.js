6ca6cc344bb8986740fda9b6f644971108e79758
Kareem Taiye <olayinkahassan117@gmail.com>
2026-02-27 07:32:15 +0000

    set up and basic auth implementation started

--- utilities/password.js
+++ utilities/password.js
@@ -4,4 +4,8 @@ export default class PasswordUtils {
   static async hashPassword(password) {
     return await bcrypt.hash(password, 12);
   }
+
+  static async compare(password, hashedPass) {
+    return await bcrypt.compare(password, hashedPass);
+  }
 }
